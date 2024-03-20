19e2bd8d022ba63d2007972da6df00975ae05dd1
Lola <116386650+Lil-papa@users.noreply.github.com>
2024-03-20 21:16:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const showFirst = document.querySelector('.first');
+
+showFirst.addEventListener('click', function() {
+    showFirst.classList.toggle('show-description')
+});
+   
+const showSecond = document.querySelector('.second');
+
+showSecond.addEventListener('click', function() {
+    showSecond.classList.toggle('show-description')
+});
+
+const showDessert = document.querySelector('.dessert');
+
+showDessert.addEventListener('click', function() {
+    showDessert.classList.toggle('show-description')
+});
